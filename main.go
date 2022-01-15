f9816fe593834bf86f9562121d1cb39c7fb09330
rymiyamoto <ry.miyamoto129@gmail.com>
2022-01-16 01:15:10 +0900

    use echo

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/gocraft/dbr/v2"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	line := dbr.NewNullString("hellow wordl")
-	fmt.Printf("%v", line)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// hello Handler.
+func hello(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Hello, World!")
 }
