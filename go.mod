7eb3bdf5df9f56b2a21f9cbc67bd8bad24f12d19
rymiyamoto <ry.miyamoto129@gmail.com>
2022-01-16 13:32:44 +0900

    use logger

--- go.mod
+++ go.mod
@@ -15,6 +15,9 @@ require (
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.20.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20210913180222-943fd674d43e // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
+	"github.com/rymiyamoto/memer-server/middleware"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func main() {
+	logger, err := zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development: true,
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			FunctionKey:    zapcore.OmitKey,
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stdout"},
+	}.Build(
+		zap.AddCaller(),
+		zap.Fields(
+			zap.String("permanent", "value"),
+		),
+	)
+	if err != nil {
+		panic(fmt.Errorf("failed to zap.Build. err: %w", err))
+	}
+
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			panic(fmt.Errorf("failed to logger.Sync. err: %w", err))
+		}
+	}()
+
 	// Echo instance
 	e := echo.New()
 
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	e.Use(middleware.ZapLogger(logger))
+	e.Use(echoMiddleware.Recover())
 
 	// Routes
 	e.GET("/", hello)
