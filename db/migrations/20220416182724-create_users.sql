da8cbeb326459c3758f28ac40d36be7444be644e
rymiyamoto <ry.miyamoto129@gmail.com>
2022-04-24 20:07:07 +0900

    fix: typo

--- db/migrations/20220416182724-create_users.sql
+++ db/migrations/20220416182724-create_users.sql
@@ -3,9 +3,9 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `id`              BIGINT       UNSIGNED AUTO_INCREMENT,
   `is_authed`       BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '認証済みであるか',
-  `name`            VARCHAR(64),                        COMMENT 'ユーザー名',
+  `name`            VARCHAR(64)                         COMMENT 'ユーザー名',
   `deleted_at`      DATETIME                            COMMENT '退会日',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
 -- +migrate Down
