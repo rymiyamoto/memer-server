707b228fc0bb6131d86a6a21501cae1e60d7ba5d
rymiyamoto <ry.miyamoto129@gmail.com>
2022-04-24 20:06:28 +0900

    fix: sql

--- db/migrations/20220416182724-create_users.sql
+++ db/migrations/20220416182724-create_users.sql
@@ -2,9 +2,9 @@
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `users` (
   `id`              BIGINT       UNSIGNED AUTO_INCREMENT,
-  `is_authed`       BOOLEAN      NOT NULL DEFAULT FALSE COMMENT='認証済みであるか',
-  `name`            VARCHAR(64),                        COMMENT='ユーザー名',
-  `deleted_at`      DATETIME                            COMMENT='退会日',
+  `is_authed`       BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '認証済みであるか',
+  `name`            VARCHAR(64),                        COMMENT 'ユーザー名',
+  `deleted_at`      DATETIME                            COMMENT '退会日',
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
